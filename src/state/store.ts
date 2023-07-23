982a7d1149d436eb2434fe1f1c560bb627207f41
Vitalick <miraigeweb@gmail.com>
2023-07-23 15:05:06 +0300

    added the localStorage.ts in the project

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,12 +1,20 @@
 import {combineReducers, legacy_createStore} from "redux";
 import {counterReducer} from "./counter-reducer";
+import {loadState, saveState} from "../utils/localStorage";
 
 export const rootReducer = combineReducers({
     counter: counterReducer
 })
 
-export const store = legacy_createStore(rootReducer)
+//здесь получаем вторым параметром инициализационное значение из того что сетает нам subscriber
+export const store = legacy_createStore(rootReducer, loadState())
 
 export type AppRootStateType = ReturnType<typeof rootReducer>
+
+//Для local storage используем паттерн observer - который принимает слушателя - callback который следит за изменениями
+//и при изменении state этот код будет отрабатывать. В этом случае сетать в localStorage какие-то значения
+store.subscribe( () => {
+    return saveState(store.getState());
+})
 // @ts-ignore
 window.store = store
